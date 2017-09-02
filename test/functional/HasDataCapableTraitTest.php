aa42c1773686aaec6254a4ddbbe07e27809a4470
Anton Ukhanev <xedin.unknown@gmail.com>
2017-09-02 21:24:48 +0200

    Added test for `HasDataCapableTrait`

---
+++ test/functional/HasDataCapableTraitTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Dhii\Data\Object\FuncTest;
+
+use Xpmock\TestCase;
+use Dhii\Data\Object\HasDataCapableTrait as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class HasDataCapableTraitTest extends TestCase
+{
+    /**
+     * The name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Data\Object\HasDataCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return object
+     */
+    public function createInstance($data = null)
+    {
+        $mock = $this->getMockForTrait(static::TEST_SUBJECT_CLASSNAME, array(), '', false, true, true, [
+            '_getDataStore',
+            '_createInvalidArgumentException',
+            '__',
+        ]);
+
+        $mock->method('_getDataStore')
+                ->will($this->returnValue($data));
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType('object', $subject, 'An instance of the test subject could not be created');
+    }
+
+    /**
+     * Tests that the reference to a data store is returned correctly.
+     *
+     * @since [*next-version*]
+     */
+    public function testHasData()
+    {
+        $key1 = 'name';
+        $data = (object) [
+            'age' => 29,
+        ];
+        $subject = $this->createInstance($data);
+        $_subject = $this->reflect($subject);
+
+        $this->assertFalse($_subject->_hasData($key1), 'Test subject initial state is wrong');
+
+        $data->{$key1} = uniqid('val1-');
+        $this->assertTrue($_subject->_hasData($key1), 'Test subject altered state is wrong');
+    }
+}
