ab5130181722d535581ea1c09bd5925a989c0b9b
Anton Ukhanev <xedin.unknown@gmail.com>
2017-10-28 21:54:23 +0200

    Increased test coverage

--- test/unit/HasDataCapableTraitTest.php
+++ test/unit/HasDataCapableTraitTest.php
@@ -30,12 +30,53 @@ class HasDataCapableTraitTest extends TestCase
     {
         $mock = $this->getMockForTrait(static::TEST_SUBJECT_CLASSNAME, array(), '', false, true, true, [
             '_getDataStore',
-            '_createInvalidArgumentException',
             '__',
         ]);
 
         $mock->method('_getDataStore')
                 ->will($this->returnValue($data));
+        $mock->method('_createInvalidArgumentException')
+                ->will($this->returnCallback(function ($message) {
+                    return $this->createInvalidArgumentException($message);
+                }));
+
+        return $mock;
+    }
+
+    /**
+     * Creates a new Invalid Argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The error message.
+     *
+     * @return InvalidArgumentException The new exception.
+     */
+    public function createInvalidArgumentException($message = '')
+    {
+        $mock = $this->getMock('InvalidArgumentException');
+
+        $mock->method('getMessage')
+                ->will($this->returnValue($message));
+
+        return $mock;
+    }
+
+    /**
+     * Creates a new stringable object.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $string The string that the object will represent.
+     *
+     * @return Stringable The new stringable.
+     */
+    public function createStringable($string)
+    {
+        $mock = $this->getMock('Dhii\Util\String\StringableInterface');
+
+        $mock->method('__toString')
+                ->will($this->returnValue($string));
 
         return $mock;
     }
@@ -70,5 +111,41 @@ class HasDataCapableTraitTest extends TestCase
 
         $data->{$key1} = uniqid('val1-');
         $this->assertTrue($_subject->_hasData($key1), 'Test subject altered state is wrong');
+    }
+
+    /**
+     * Tests that unsetting data works correctly correctly when using a stringable object as key.
+     *
+     * @since [*next-version*]
+     */
+    public function testHasDataStringable()
+    {
+        $key = 'name';
+        $stringable = $this->createStringable($key);
+        $data = (object) [
+            'age' => 29,
+        ];
+        $subject = $this->createInstance($data);
+        $_subject = $this->reflect($subject);
+
+        $this->assertFalse($_subject->_hasData($stringable), 'Test subject initial state is wrong');
+
+        $data->{$key} = uniqid('val1-');
+        $this->assertTrue($_subject->_hasData($stringable), 'Test subject altered state is wrong');
+    }
+
+    /**
+     * Tests that unsetting data fails correctly when using invalid key.
+     *
+     * @since [*next-version*]
+     */
+    public function testHasDataInvalidKeyFailure()
+    {
+        $key = new \stdClass();
+        $subject = $this->createInstance((object)[]);
+        $_subject = $this->reflect($subject);
+
+        $this->setExpectedException('InvalidArgumentException');
+        $this->assertFalse($_subject->_hasData($key), 'Test subject initial state is wrong');
     }
 }
