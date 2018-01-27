487a35912d102bec17dba64a98f761731bb1c6a7
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-27 15:16:29 +0100

    `GetDataStoreCapableTrait` now uses data store factory

--- src/GetDataStoreCapableTrait.php
+++ src/GetDataStoreCapableTrait.php
@@ -3,6 +3,8 @@
 namespace Dhii\Data\Object;
 
 use ArrayObject;
+use InvalidArgumentException;
+use stdClass;
 
 /**
  * Functionality for retrieval of the data store.
@@ -30,7 +32,20 @@ trait GetDataStoreCapableTrait
     protected function _getDataStore()
     {
         return $this->dataStore === null
-                ? $this->dataStore = new ArrayObject()
+                ? $this->dataStore = $this->_createDataStore()
                 : $this->dataStore;
     }
+
+    /**
+     * Creates a new data store.
+     *
+     * @since [*next-version*]
+     *
+     * @param stdClass|array|null $data The data for the store, if any.
+     *
+     * @throws InvalidArgumentException If the type of data for the store is invalid.
+     *
+     * @return ArrayObject The new data store.
+     */
+    abstract protected function _createDataStore($data = null);
 }
